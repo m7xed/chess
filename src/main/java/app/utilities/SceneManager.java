48fd1709c281369262ccb2be9c0bc129cd58e617
m7xed <mrfieldcs@outlook.com>
2025-04-27 19:10:44 +0100

    Remove .idea and other ignored files

--- src/main/java/app/utilities/SceneManager.java
+++
@@ -1,4 +0,0 @@
-package app.utilities;
-
-public class SceneManager {
-}
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module m7xed.chess {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens app to javafx.fxml;
-    exports app;
-}
